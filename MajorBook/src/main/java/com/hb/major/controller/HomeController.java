a922a4b5b706679035ef8dd7ebadbdd6c46e210d
beatcampsh06@gmail.com <35362702+BeatProject06@users.noreply.github.com>
2018-01-15 12:45:20 +0900

    재업 로그인(자동앱연결=가입), 로그아웃, 앱연결해지(탈퇴) 기능 임시

---
+++ MajorBook/src/main/java/com/hb/major/controller/HomeController.java
@@ -0,0 +1,74 @@
+package com.hb.major.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hb.major.model.entity.UserVo;
+import com.hb.major.model.user.UserDaoImpl;
+
+
+@Controller
+public class HomeController {
+   
+   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+   
+   UserVo bean;
+   
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String home(Locale locale) {
+      logger.info("The client locale is {}.", locale);
+
+      return "home";
+   }
+   
+   
+   
+   
+   @RequestMapping(value = "/afterlogin/{kkoid}", method = RequestMethod.POST)
+   public String login(@PathVariable("kkoid") String kkoid, Model model, HttpServletRequest req) throws Exception {
+
+		req.setCharacterEncoding("UTF-8");
+
+		System.out.println(req.getParameter("kakao_id"));
+		System.out.println(req.getParameter("kakao_nick"));
+		
+		int id = Integer.parseInt(req.getParameter("kakao_id").substring(1, 7));
+		String nickname = req.getParameter("kakao_nick");
+		
+		UserDaoImpl userDaoImpl = new UserDaoImpl();
+//		디버깅 중 아직 안됨
+//		bean.setNum(1);
+//		bean.setId(id);
+//		bean.setNickname(nickname);
+//		System.out.println(bean);
+//		
+//		userDaoImpl.userInsertOne(bean);
+		
+		return "afterlogin";
+   }
+   
+   
+   
+	@RequestMapping(value="/unlinkapp", method=RequestMethod.POST)
+	public String unlink() {
+		//db에서 아이디 삭제해야하니까 post방식으로
+		
+		return "unlinkapp"; //사실 여기 리다이렉트줘야하지 않나 싶은....
+	}
+	
+	
+   
+   
+   
+   
+   
+}
