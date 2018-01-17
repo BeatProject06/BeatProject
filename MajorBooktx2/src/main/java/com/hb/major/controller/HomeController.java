62fa0b75a32da70ae58c49030104bd624cae68f2
beatcampsh06@gmail.com <35362702+BeatProject06@users.noreply.github.com>
2018-01-17 09:24:51 +0900

    restful로 하면 css 깨짐

---
+++ MajorBooktx2/src/main/java/com/hb/major/controller/HomeController.java
@@ -0,0 +1,111 @@
+package com.hb.major.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hb.major.model.entity.BbsVo;
+import com.hb.major.model.entity.UserVo;
+import com.hb.major.service.bbs.BbsService;
+import com.hb.major.service.user.UserService;
+
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	@Autowired private UserService userService;
+	
+	@Autowired private BbsService bbsService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale) {
+		logger.info("The client locale is {}.", locale);
+
+		return "home";
+	}
+
+	
+	
+	@RequestMapping(value = "/board", method = RequestMethod.GET)
+	public String boardfirstview(Locale locale, Model model) {
+		
+		logger.info("게시판", locale);
+		bbsService.bbsListAll(model);
+		
+		return "user/board";
+	}
+	
+	
+	@RequestMapping(value = "/board/{bbspage}", method = RequestMethod.GET)
+	public String board(Locale locale, Model model, @PathVariable("bbspage") int bbspage) {
+		
+		logger.info("게시판", locale);
+		System.out.println("게시판 페이지는 "+bbspage);
+		bbsService.bbsListAll(model);
+		
+		return "user/board";
+	}
+	
+	@RequestMapping(value = "/board/detail/{postno}", method = RequestMethod.GET)
+	public String detail(Locale locale, Model model, @PathVariable("postno") int postno) {
+		
+		logger.info("게시글 상세 페이지", locale);
+		
+		bbsService.bbsDetailOne(postno, model);
+		
+		return "user/detail";
+	}
+
+	@RequestMapping(value = "/board/write", method = RequestMethod.GET)
+	public String write(Locale locale, Model model) {
+		logger.info("게시글 작성페이지", locale);
+		System.out.println("add 겟 들어옴");
+		return "user/write";
+	}
+
+	@RequestMapping(value = "/board/write", method = RequestMethod.POST)
+	public String writeadd(Locale locale, @ModelAttribute BbsVo bean, HttpServletRequest req) {
+		
+		logger.info("게시글 작성", locale);
+		System.out.println("add 포스트 들어옴");
+		try {
+			req.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		bbsService.bbsAddOne(bean);
+		System.out.println(bean);
+		
+		return "redirect:/board";
+	}
+
+//	@RequestMapping(value = "/afterlogin/{kkoid}", method = RequestMethod.POST)
+//	public String login(@PathVariable("kkoid") String kkoid, @ModelAttribute UserVo bean, HttpServletRequest req) throws Exception {
+//
+//		req.setCharacterEncoding("UTF-8");
+//
+//		bean.setKakao_id(req.getParameter("kakao_id"));
+//		bean.setKakao_nick(req.getParameter("kakao_nick"));
+//		bean.setProfile_img(req.getParameter("kakao_profile_image"));
+//		bean.setThumb_img(req.getParameter("kakao_thumbnail_image"));
+//		System.out.println(bean);
+//		
+////		userService.userInsertOne(bean);
+////		System.out.println("입력");
+//		
+//		return "afterlogin";
+//	}
+
+}
