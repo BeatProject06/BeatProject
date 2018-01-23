92deaff6cb5b5ec888bb2602d1d76459ce92071e
TaeKyeong <tk0720@naver.com>
2018-01-23 15:11:59 +0900

    문의사항 question 게시판 및 댓글 dao, service 생성

---
+++ MajorBook/src/main/java/com/hb/major/model/qna/comment/QnaCommDaoImpl.java
@@ -0,0 +1,26 @@
+package com.hb.major.model.qna.comment;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.hb.major.model.entity.QnaCommentVo;
+
+public class QnaCommDaoImpl implements QnaCommDao {
+	
+	SqlSession session;
+	public void setSession(SqlSession session) {
+		this.session = session;
+	}
+
+	@Override
+	public List<QnaCommentVo> qnaGetCommentList(int postnum) throws Exception {
+		return session.selectList("com.hb.major.model.qna.comment.QnaCommDao.qnaGetCommentList", postnum);
+	}
+
+	@Override
+	public void qnaCommentInsertOne(QnaCommentVo cmtbean) throws Exception {
+		session.insert("com.hb.major.model.qna.comment.QnaCommDao.qnaCommentInsertOne", cmtbean);
+	}
+
+}
